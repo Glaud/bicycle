1a8e95a52a4c3e04aa368150d11a238146d72358
Pawel Karys <p.karys11@gmail.com>
2023-06-18 17:30:02 +0200

    fix contextLoad test

--- src/main/java/lv/proofit/bicycle/ApplicationStartup.java
+++ src/main/java/lv/proofit/bicycle/ApplicationStartup.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +16,7 @@ import java.util.*;
 
 @Component
 @Slf4j
+@Profile("!test")
 public class ApplicationStartup {
 
     @Value("${groovy.scripts.path}")
