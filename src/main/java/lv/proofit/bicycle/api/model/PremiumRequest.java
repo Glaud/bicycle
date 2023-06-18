97b3a9c298b70e3002731f045fb7432a0fa25981
Pawel Karys <p.karys11@gmail.com>
2023-06-18 16:43:57 +0200

    add bicycle list validation

--- src/main/java/lv/proofit/bicycle/api/model/PremiumRequest.java
+++ src/main/java/lv/proofit/bicycle/api/model/PremiumRequest.java
@@ -1,8 +1,9 @@
 package lv.proofit.bicycle.api.model;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotEmpty;
 
 import java.util.List;
 
-public record PremiumRequest(@Valid List<BicyclePolicy> bicycles) {
+public record PremiumRequest(@Valid @NotEmpty List<BicyclePolicy> bicycles) {
 }
